7b2223d5289f6d271a19e9454e8dbae0e1f84e46
dv <veresovdanil@gmail.com>
2025-10-19 17:54:06 +0000

    feat: создание endpoint для дергания обновлений из crm

--- index.php
+++ index.php
@@ -9,26 +9,4 @@ $dotenv->load();
 
 $botToken = $_ENV['BOT_TOKEN'];
 $chatId = $_ENV['TELEGRAM_CHAT_ID'];
-$sendMessage = "Привет, я первое сообщение из бота";
-
-try {
-
-    $client = new \GuzzleHttp\Client();
-
-    $response = $client->post('https://api.telegram.org/bot' . $botToken . "/sendMessage", [
-        'form_params' => [
-            'chat_id' => $chatId,
-            'text' => $sendMessage,
-        ]
-    ]);
-
-    echo $chatId;
-    echo "<br>";
-    echo "Статус ответа: " . $response->getStatusCode();
-    echo "<br>";
-    echo "Тело ответа: " . $response->getBody();
-
-} catch (GuzzleException $e) {
-    echo $e->getMessage();
-}
 
