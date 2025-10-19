b030b19f666fa9732f5cf18c79c44c3ce0932610
dv <veresovdanil@gmail.com>
2025-10-19 17:52:37 +0000

    feat: протестировал что отправленные мной сообщения приходят в бота

--- index.php
+++ index.php
@@ -1,6 +1,34 @@
 <?php
 
+use GuzzleHttp\Exception\GuzzleException;
+
+require __DIR__ . '/vendor/autoload.php';
+
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$botToken = getenv('BOT_TOKEN');
+$botToken = $_ENV['BOT_TOKEN'];
+$chatId = $_ENV['TELEGRAM_CHAT_ID'];
+$sendMessage = "Привет, я первое сообщение из бота";
+
+try {
+
+    $client = new \GuzzleHttp\Client();
+
+    $response = $client->post('https://api.telegram.org/bot' . $botToken . "/sendMessage", [
+        'form_params' => [
+            'chat_id' => $chatId,
+            'text' => $sendMessage,
+        ]
+    ]);
+
+    echo $chatId;
+    echo "<br>";
+    echo "Статус ответа: " . $response->getStatusCode();
+    echo "<br>";
+    echo "Тело ответа: " . $response->getBody();
+
+} catch (GuzzleException $e) {
+    echo $e->getMessage();
+}
+
