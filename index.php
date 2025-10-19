e5fa4abb930a62e0f774be1a8a719e6a559fbb61
dv <veresovdanil@gmail.com>
2025-10-19 22:58:10 +0000

    feat: добавил логирование и роутинг вместо простого отправления в чат текстового сообщения

--- index.php
+++ index.php
@@ -10,3 +10,17 @@ $dotenv->load();
 $botToken = $_ENV['BOT_TOKEN'];
 $chatId = $_ENV['TELEGRAM_CHAT_ID'];
 
+$requestUri = $_SERVER['REQUEST_URI'];
+
+if ($_SERVER['REQUEST_URI'] === "/") {
+    echo "Bot is already";
+} elseif (str_starts_with($requestUri, "/webhook/crm")) {
+    $requestBody = file_get_contents("php://input");
+
+
+    file_put_contents(
+        __DIR__ . "/logs/crm.log",
+        $requestBody . "\n",
+        FILE_APPEND
+    );
+}
