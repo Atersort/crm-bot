21ae87d8dc4fa5921703c296b8c7a9cea215237f
dV <veresovdanil@gmail.com>
2025-11-20 21:43:13 +0300

    feat: crm handlers class create

--- src/Services/TelegramService.php
+++ src/Services/TelegramService.php
@@ -1,8 +1,36 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Services;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Dotenv\Dotenv;
+
 class TelegramService
 {
+
+public object $client;
+public string $chatId;
+public string $token;
+
+public function __construct() {
+    $this->client = new Client();
+    $this->chatId = $_ENV['TELEGRAM_CHAT_ID'];
+    $this->token = $_ENV['TELEGRAM_TOKEN'];
+}
+
+public function sendMessage(string $message) {
+    try {
+        $responce = $this->client->post("https://api.telegram.org/bot{$this->token}/sendMessage", [
+            'form_params' => [
+                'chat_id' => $this->chatId,
+                'text' => $message,
+            ]
+        ]);
+    } catch (GuzzleException $e) {
+        file_put_contents('error.log', $e->getMessage() . PHP_EOL . "\n", FILE_APPEND);
+    }
+}
 
 }
