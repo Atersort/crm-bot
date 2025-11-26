548cb4f62667a2b5ba902b12ca7d05660ead9664
dV <veresovdanil@gmail.com>
2025-11-27 01:04:56 +0300

    feat: создание функционала отправки сообщений пользователям

---
+++ src/Config/users.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    // id_CRM => id_Telegram
+    397284 => 338147292, // dv
+    391505 => 5474621307 // dv_work
+];
--- src/Handlers/CrmHandler.php
+++ src/Handlers/CrmHandler.php
@@ -21,11 +21,16 @@ class CrmHandler
 
         $id = $this->data['current']['id'];
         $name = $this->data['current']['name'];
+        $id_task = $this->data['current']['id'];
+
+        $created_date = $this->data['current']['created_date'];
+        $updated_date = $this->data['current']['updated_date'];
 
         $event = $this->data['current']['type'];
 
-        if ($this->data['current']['created_date'] === $this->data['current']['updated_date']) {
-            return "✅ **Создана новая задача:** #$id\n📝 $name";
+        if ($created_date === $updated_date) {
+            return "✅ **Создана новая задача:** #$id\n📝 $name\n url: [Ссылка](https://6k4cmc691.aspro.cloud/_module/task/view/task/$id_task)
+            " ;
         } else {
             return "🔄 **Обновлена задача:** #$id\n📝 $name";
         }
